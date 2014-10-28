3dfbc0408852d2e208dd52ab55184f9fe0f415e9
bbusen <bbusen@illinois.edu>
2014-10-27 19:54:47 -0500

    Solar declination added

--- shine.py
+++ shine.py
@@ -1,2 +1,5 @@
 def shine(time_is, timestep=1):
 	hr_angle_rad = pi * (gmt - longitude  / 15)  / 12
+	term1 = 2 * (time_is.timetuple().yday + 10) / Earth.loy
+	term2 = 2 * Earth.ecc * sin( 2 * ( time_is.timetuple().yday - 2 ) / Earth.loy )
+	solar_declination = -asin(Earth.sin_incl * cos(term1 + term2))
