f3d856b8e1248e773ef37e235b9ed278228d85e0
bbusen <bbusen@illinois.edu>
2014-11-03 03:07:07 -0600

    Update Planet.py

--- Planet.py
+++ Planet.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+
+
 class Planet:
 
   length_of_year_in_days = 365.25
@@ -5,11 +9,15 @@ class Planet:
   inclination_deg = -23.44
   solar_declination = 0
   solar_constant_W_per_m2 = 1361
-  
-    def __init__(self, name):
+  current_time = datetime(2012, 6, 12, 0, 0)
+
+    def __init__(self, name, specified_time):
         self.name = name
         self.region_list = []
+        self.current_time = specified_time
+        self.solar_declination = -asin()
         
-    def set_time(self, current_time):
+    def set_time(self, specified_time):
+        current_time = specified_time
     
     def rotate(self, timestep, num_steps):
