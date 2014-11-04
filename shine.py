93add2a2effae7127819f74cacb1b45449d49b9b
bbusen <bbusen@illinois.edu>
2014-11-04 04:54:41 -0600

    Update shine.py

--- shine.py
+++ shine.py
@@ -1,4 +1,4 @@
-def shine(time_is, timestep=1):
+def shine(self, user):
 	hr_angle_rad = pi * (gmt - longitude  / 15)  / 12
 	term1 = 2 * (time_is.timetuple().yday + 10) / Earth.loy
 	term2 = 2 * Earth.ecc * sin( 2 * ( time_is.timetuple().yday - 2 ) / Earth.loy )
