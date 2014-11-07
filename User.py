68fb1849f827c554f5b552f2084ec534747be848
bbusen <bbusen@illinois.edu>
2014-11-07 00:01:50 -0600

    Create User.py

---
+++ User.py
@@ -0,0 +1,4 @@
+class User:
+  display_output_labels = ["Sunlight", Temperature", "Barometric Pressure", "Relative Humidity"]
+  display_output_selected = [true, false, false, false]
+  display_output_units = ["W/m2", "deg F", "millibars", "%"]
