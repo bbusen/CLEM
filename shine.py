94b41c7b877169de950ae87e649f4ee8fcd08e01
bbusen <bbusen@illinois.edu>
2014-11-07 20:10:35 -0600

    Delete shine.py
    Should be a method of the Earth object

--- shine.py
+++
@@ -1,5 +0,0 @@
-def shine(self, user):
-	hr_angle_rad = pi * (gmt - longitude  / 15)  / 12
-	term1 = 2 * (time_is.timetuple().yday + 10) / Earth.loy
-	term2 = 2 * Earth.ecc * sin( 2 * ( time_is.timetuple().yday - 2 ) / Earth.loy )
-	solar_declination = -asin(Earth.sin_incl * cos(term1 + term2))
